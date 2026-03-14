9e575a7f6919ea3182ad9382eb7982ef112d9220
plalwa69 <cheeaaaaters26@gmail.com>
2026-03-15 02:51:10 +0530

    Add files via upload

---
+++ OddEvenServer.java
@@ -0,0 +1,43 @@
+
+
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+
+public class OddEvenServer {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/check", (HttpExchange exchange) -> {
+            URI requestURI = exchange.getRequestURI();
+            String query = requestURI.getQuery();
+            String response = "Invalid Request";
+
+            if (query != null && query.startsWith("number=")) {
+                try {
+                    int number = Integer.parseInt(query.split("=")[1]);
+                    if (number % 2 == 0) {
+                        response = number + " is Even";
+                    } else {
+                        response = number + " is Odd";
+                    }
+                } catch (NumberFormatException e) {
+                    response = "Please provide valid number";
+                }
+            }
+
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+        System.out.println("Server started on port 8080");
+    }
+}
